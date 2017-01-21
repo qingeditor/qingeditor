f3c5559193e9fe1ec12ac4f5da734946071ce4d5
softboy <zzu_softboy@163.com>
2017-01-22 06:56:10 +0800

    完成qingeditor/hash-table/set方法和qingeditor/hash-table/get方法的单元测试

--- tests/ds/ds-hash-table-test.el
+++ tests/ds/ds-hash-table-test.el
@@ -25,6 +25,46 @@
   (let ((ht (qingeditor/hash-table/init)))
     (should (object-of-class-p ht qingeditor/hash-table))))
 
+(ert-deftest qingeditor/test/ds/hash-table-set-test ()
+  :tags '(qingeditor/ds/hash-table/set)
+  "`hash-table'的`set'函数测试。"
+  (qingeditor/test/ds/prepare-hash-table
+   (lambda ()
+     (let ((table (oref ht :table))
+	   (new-table (make-hash-table)))
+       ;; 普通的值
+       (qingeditor/hash-table/set ht 'name "softboy")
+       (qingeditor/hash-table/set ht 'age 12)
+       ;; 存放一下`list'
+       (qingeditor/hash-table/set ht 'props '(a b c))
+       ;; 存放一下`hash-table'
+       (qingeditor/hash-table/set ht 'data new-table)
+       (should (equal (hash-table-count table) 4))
+       (should (equal (gethash 'name table) "softboy"))
+       (should (equal (gethash 'age table) 12))
+       (should (equal (gethash 'props table) '(a b c)))
+       (should (eq (gethash 'data table) new-table))))))
+
+(ert-deftest qingeditor/test/ds/hash-table-get-test ()
+  :tags '(qingeditor/ds/hash-table/get)
+  "`hash-table'的`get'函数测试。"
+  (qingeditor/test/ds/prepare-hash-table
+   (lambda ()
+     (let ((table (oref ht :table))
+	   (new-table (make-hash-table)))
+       ;; 普通的值
+       (qingeditor/hash-table/set ht 'name "softboy")
+       (qingeditor/hash-table/set ht 'age 12)
+       ;; 存放一下`list'
+       (qingeditor/hash-table/set ht 'props '(a b c))
+       ;; 存放一下`hash-table'
+       (qingeditor/hash-table/set ht 'data new-table)
+       (should (equal (qingeditor/hash-table/get ht 'name) "softboy"))
+       (should (equal (qingeditor/hash-table/get ht 'age) 12))
+       (should (equal (qingeditor/hash-table/get ht 'props) '(a b c)))
+       (should (eq (qingeditor/hash-table/get ht 'data) new-table))
+       (should (equal (qingeditor/hash-table/get ht 'not-exist "haha") "haha"))))))
+
 
 
 
