1604295c6595f8b83059f414e72ba586f417ddd6
softboy <zzu_softboy@163.com>
2017-02-01 14:07:55 +0800

    完成qingeditor/eventmgr/listener-aggregate类

---
+++ libs/eventmgr/qingeditor-eventmgr-listener-aggregate.el
@@ -0,0 +1,28 @@
+;; Copyright (c) 2016-2017 zzu_softboy & Contributors
+;;
+;; qingeditor (www.qingeditor.org)
+;; Author: zzu_softboy <zzu_softboy@163.com>
+;; Github: https://www.github.com/qingeditor/qingeditor
+;;
+;; This file is not part of GNU Emacs.
+;; License: MIT
+;;
+;; 抽象的`listener'类
+
+(defmethod qingeditor/eventmgr/listener-aggregate ()
+  ((listeners
+    :initarg :listeners
+    :initform nil
+    :type list
+    :documentation "`qingeditor/eventmgr/event-handler'集合。"))
+  :abstract t
+  :documentation "事件监听类。")
+
+(defmethod qingeditor/eventmgr/listener-aggregate/detach
+  ((this qingeditor/eventmgr/listener-aggregate) (eventmgr qingeditor/eventmgr/mgr))
+  "移除事件监听处理器。"
+  (dolist (handler (oref this :listeners))
+    (qingeditor/eventmgr/mgr/detach eventmgr handler))
+  (oset this :listeners nil))
+
+(provide 'qingeditor-eventmgr-listener-aggregate)
